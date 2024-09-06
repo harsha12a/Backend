58529c8380a684763aa740735ad89730c9cf797f
Sree Satya Harsha <harsha.pss2005@gmail.com>
2024-09-06 19:14:39 +0530

    working cruds

--- APIs/userApi.js
+++ APIs/userApi.js
@@ -1,14 +1,17 @@
 const exp = require('express');
 const userApp=exp.Router();
 const User=require('../Models/userModel')
+const bcrypt = require('bcrypt');
+const expressAsync=require('express-async-handler')
 
-userApp.get('/',async(req,res)=>{
+
+userApp.get('/',expressAsync(async(req,res)=>{
     const users=req.app.get('users')
     let arr=await users.find().toArray()
     res.send({message:'Hello User',payload:arr})
-})
+}))
 
-userApp.get('/:nameUrl',async(req,res)=>{
+userApp.get('/:nameUrl',expressAsync(async(req,res)=>{
     const users=req.app.get('users')
     let {nameUrl}=req.params
     let user=await users.findOne({name:nameUrl})
@@ -17,14 +20,15 @@ userApp.get('/:nameUrl',async(req,res)=>{
     }
     else
         res.send({message:'Hello User',payload:user})
-})
+}))
 
-userApp.post('/register',async(req,res)=>{
+userApp.post('/register',expressAsync(async(req,res)=>{
     const users=req.app.get('users')
     let data=req.body
     
     try{
         let obj=new User(data)
+        obj.password=await bcrypt.hash(obj.password,10)
         await obj.save()
         res.send({message:'User registered',payload:data})
     }
@@ -32,9 +36,9 @@ userApp.post('/register',async(req,res)=>{
         console.log(err);
         res.send({message:'Error occured',error:err.message})
     }
-})
+}))
 
-userApp.post('/login',async(req,res)=>{
+userApp.post('/login',expressAsync(async(req,res)=>{
     const users=req.app.get('users')
     let {nameUrl,password}=req.body
     let user=await users.findOne({name:nameUrl})
@@ -42,27 +46,28 @@ userApp.post('/login',async(req,res)=>{
         res.send({message:'User not found'})
     }
     else{
-        if(password===user.password)
+        let res1=await bcrypt.compare(password,user.password)
+        if(res1===true)
             res.send({message:'Hello User',payload:user})
         else
             res.send({message:'Invalid credentials'})
     }
-})
+}))
 
-userApp.put('/update',async(req,res)=>{
+userApp.put('/update',expressAsync(async(req,res)=>{
     const users=req.app.get('users')
-    let nameUrl=req.body
-    let user=await users.findOne({name:{$eq:nameUrl}})
+    let newUser=req.body
+    let user=await users.findOne({name:{$eq:newUser.nameUrl}})
     if(user===null){
         res.send({message:'User not found'})
     }
     else{
-        await users.updateOne({name:{$eq:nameUrl}},{$set:{...user}})
-        res.send({message:'User updated',payload:users})
+        await users.updateOne({name:{$eq:newUser.nameUrl}},{$set:{...newUser}})
+        res.send({message:'User updated',payload:newUser})
     }
-})
+}))
 
-userApp.delete('/delete/:nameUrl',(req,res)=>{
+userApp.delete('/delete/:nameUrl',expressAsync((req,res)=>{
     const users=req.app.get('users')
     let nameUrl=req.params.nameUrl
     let user=users.findOne({name:nameUrl})
@@ -73,6 +78,6 @@ userApp.delete('/delete/:nameUrl',(req,res)=>{
             users.deleteOne({name:nameUrl})
             res.send({message:'User deleted'})
     }
-})
+}))
 
 module.exports=userApp;
