186ac669313a0384b1423d5978b6ea16061e395e
Sree Satya Harsha <harsha.pss2005@gmail.com>
2024-10-10 17:06:21 +0530

    add socket.io

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const exp = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const {MongoClient}=require('mongodb')
+require('dotenv').config()
+// require('./db')
+
 const app=exp()
 app.use(exp.json())
-require('dotenv').config()
-const {MongoClient}=require('mongodb')
-require('./db')
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('message', (data) => {
+        console.log('Message from client:', data);
+        socket.emit('response', { message: 'Message received!', data });
+        io.emit('message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+    });
+});
 
 const port=(process.env.PORT)
 const userApp = require('./APIs/userApi');
@@ -15,7 +42,7 @@ mClient.connect().then((obj)=>{
     let users=flat.collection('users')
     app.set('users',users)
     console.log('Connected to db');
-    app.listen(port,()=>{
+    server.listen(port,()=>{
         console.log(`Port at ${port}`);
     })
 }).catch((err)=>{
